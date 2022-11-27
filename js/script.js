e7d154841c0d2d6ebda4ea884856d6051397ed97
asirko1988 <112867390+asirko1988@users.noreply.github.com>
2022-11-27 18:37:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*let user; let userName = 'Вася'
+user = userName;
+console.log(user);
+*/
+
+/*let userAge = 36;
+let userInfo = "Фрилансер ${userAge}";
+*/
+
+/*let userHeight = 145 / 0;
+console.log(userHeight);
+*/
+
+/*let userName;
+console.log(typeof userName);
+*/
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
